b2c47c0ac2e71501c75ee44b3de526068749f0d9
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2022-09-30 05:17:07 -0700

    thebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char rev;
+
+	for (rev = 122; rev >= 97; rev--)
+		putchar(rev);
+	putchar('\n');
+	return (0);
+}
