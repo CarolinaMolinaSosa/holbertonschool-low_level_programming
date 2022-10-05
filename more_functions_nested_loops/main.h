a1d7d16b6be9fd86e1a895d68729574efaff34f4
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2022-10-05 05:41:24 -0700

    fix

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,10 +1,14 @@
-#ifndef main_h
-#define main_h
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * _isupper - function checks whether a character is an uppercase alphabet
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -22,4 +26,9 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-#endif
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
