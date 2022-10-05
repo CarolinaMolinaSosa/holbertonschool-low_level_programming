32aeb88c06252f5ef8722ff58fe5592c7a2a30b4
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2022-10-05 05:16:37 -0700

    fixed

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,41 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
-<<<<<<< HEAD
-
-#include <stdio.h>
+#ifndef main_h
+#define main_h
 #include <unistd.h>
-
-=======
-/*libraries*/
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-#include <unistd.h>
-/*prototypes*/
->>>>>>> ff1fc5f8d65feb8c8f01231258a05578cb77f425
 
 int _putchar(char c);
-
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 int _isupper(int c);
-
 int _isdigit(int c);
-
 int mul(int a, int b);
-
 void print_numbers(void);
-
 void print_most_numbers(void);
-
-void more_numbers(void);
-
-void print_line(int n);
-
-void print_diagonal(int n);
-
-void print_square(int size);
-
-void print_triangle(int size);
-
-<<<<<<< HEAD
 #endif
-=======
-#endif /*main.h*/
->>>>>>> ff1fc5f8d65feb8c8f01231258a05578cb77f425
