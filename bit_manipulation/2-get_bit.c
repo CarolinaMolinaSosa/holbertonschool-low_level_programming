ce469f79abc3220d1bb6bc8f1c247b46fd99ce33
CarolinaMolinaSosa <molinasosacarolina17@gmail.com>
2022-11-21 06:39:24 -0800

    fixed typo

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * get_bit - get the value of a bit at a given index
- * @n: number 
+ * @n: number
  * @index: is the index, starting from 0 of the bit you want to get
  *
  * Return: the value of the bit at index index or -1 if an error occured
